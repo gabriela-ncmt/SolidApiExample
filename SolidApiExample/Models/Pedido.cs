398eb031364f466990707b2dfe8fb4b0278f966f
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-02-03 11:25:36 -0300

    feat: creating model

---
+++ SolidApiExample/Models/Pedido.cs
@@ -0,0 +1,9 @@
+﻿namespace SolidApiExample.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public decimal Valor { get; set; }
+        public string FormaPagamento { get; set; }
+    }
+}
