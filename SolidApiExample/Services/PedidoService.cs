a5d2b9ddb765517aa802b77f1fdee6371b1863c3
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-02-03 11:31:33 -0300

    feat: appling the Single Responsiblity Principle
    explained: service class that only cares about creating the order. The payment logic will be delegated to the payment strategies.

---
+++ SolidApiExample/Services/PedidoService.cs
@@ -0,0 +1,20 @@
+﻿using SolidApiExample.Models;
+
+namespace SolidApiExample.Services
+{
+    public class PedidoService
+    {
+        private readonly IPagamentoStrategy _pagamentoStrategy;
+
+        public PedidoService(IPagamentoStrategy pagamentoStrategy)
+        {
+            _pagamentoStrategy = pagamentoStrategy;
+        }
+
+        private void CriarPedido(Pedido pedido)
+        {
+            Console.WriteLine($"Pedido {pedido.Id} de R${pedido.Valor} criado com sucesso!");
+            _pagamentoStrategy.ProcessarPagamento(pedido);
+        }
+    }
+}
