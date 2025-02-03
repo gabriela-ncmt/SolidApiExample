6d5f5eb0eb008183306b06ee9e08e31124f7ee64
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-02-03 11:54:06 -0300

    feat: injecting dependencies into the program

--- SolidApiExample/Program.cs
+++ SolidApiExample/Program.cs
@@ -1,11 +1,18 @@
+using SolidApiExample.PaymentStrategies;
+using SolidApiExample.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSingleton<PedidoService>();
+builder.Services.AddSingleton<IPagamentoStrategy, PagamentoCredito>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
